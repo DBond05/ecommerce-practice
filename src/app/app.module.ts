99eea5bc4c66a6d7eb5d21b1df5cab73694d8e3a
DBond05 <dbond.tech@gmail.com>
2023-08-22 05:07:18 -0500

    Auth Inteceptor security update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
-import { HttpClientModule} from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import { ProductService } from './services/product.service';
 import { PrductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { SearchComponent } from './components/search/search.component';
@@ -21,6 +21,7 @@ import myAppConfig from './config/my-app-config';
 import { OktaAuth } from '@okta/okta-auth-js';
 import { MembersPageComponent } from './components/members-page/members-page.component';
 import { OrderHistoryComponent } from './components/order-history/order-history.component';
+import { AuthInterceptorService } from './services/auth-interceptor.service';
 
 const oktaConfig = myAppConfig.oidc;
 const oktaAuth = new OktaAuth(oktaConfig);
@@ -48,7 +49,8 @@ const oktaAuth = new OktaAuth(oktaConfig);
     ReactiveFormsModule,
     OktaAuthModule
   ],
-  providers: [ProductService, {provide: OKTA_CONFIG, useValue: {oktaAuth}}],
+  providers: [ProductService, {provide: OKTA_CONFIG, useValue: {oktaAuth}},
+                              {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/auth-interceptor.service.ts
@@ -0,0 +1,33 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { OKTA_AUTH } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
+import { Observable, from, lastValueFrom } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthInterceptorService implements HttpInterceptor{
+
+  constructor(@Inject(OKTA_AUTH) private oktaAuth: OktaAuth) { }
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    
+    return from(this.handleAccess(request,next));
+  }
+  private async handleAccess(request: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>> {
+    //array of secured endpoints
+    const securedEndpoints= ['http://localhost:8080/api/orders'];
+    if (securedEndpoints.some(url => request.urlWithParams.includes(url))){
+      //if matches get access token
+      const accessToken = this.oktaAuth.getAccessToken();
+      // request is immutable (cant be changed) so...
+      //clone request add new header with access token
+      request = request.clone({
+        setHeaders: {
+          Authorization: 'Bearer '+ accessToken
+        }
+      })
+    }
+    return await lastValueFrom(next.handle(request));
+  }
+}
